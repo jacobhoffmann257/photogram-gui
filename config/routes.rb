067b1fa63c530a500934672f94e1dc6a0fd04694
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-10-05 20:02:54 +0000

    comment delete done

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ get("/users", {controller: "users", action: "index"})
 get("/users/:path_username",{controller: "users", action: "show"})
 get("/photos", {controller: "photos", action: "index"})
 get("/photos/:path_id",{ controller: "photos", action: "show"})
-post("delete_photo/:photo_id",{controller: "photos", action: "delete"})
+post("delete_photo/:path_id",{controller: "photos", action: "delete"})
 end
