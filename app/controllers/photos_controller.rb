0f02755c90bb18be3e38ceef042ffe61c007df9c
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-10-05 20:21:32 +0000

    create done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,4 +15,15 @@ class PhotosController < ApplicationController
     photo.destroy
     redirect_to("/photos")
   end
+  def create
+  input_image = params.fetch("query_image")
+  input_caption= params.fetch("query_caption")
+  input_owner_id = params.fetch("query_owner_id")
+  new_photo = Photo.new
+  new_photo.image = input_image
+  new_photo.caption = input_caption
+  new_photo.owner_id = input_owner_id
+  new_photo.save
+  redirect_to("/photos/"+new_photo.id.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ get("/users", {controller: "users", action: "index"})
 get("/users/:path_username",{controller: "users", action: "show"})
 get("/photos", {controller: "photos", action: "index"})
 get("/photos/:path_id",{ controller: "photos", action: "show"})
-post("delete_photo/:path_id",{controller: "photos", action: "delete"})
+post("/delete_photo/:path_id",{controller: "photos", action: "delete"})
+post("/create_photo",{controller: "photos", action: "create"})
 end
