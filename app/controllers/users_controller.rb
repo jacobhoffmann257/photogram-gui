5ba204a31daa68c452f119366b982a2c7b18225f
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-10-05 18:56:41 +0000

    user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     @list_of_users = matching_users.order({:username => :asc})
     render({:template => "users_templates/index"})
   end
+  def show
+  path_name = params.fetch("path_username")
+  @current_user = User.where({username: path_name}).first
+ # if the_user == nil
+  #  redirect_to("/404", status:404)
+  #else 
+    render({template: "users_templates/show"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #need to change these to posts
+get("/", {controller: "users", action: "index"})
 get("/users", {controller: "users", action: "index"})
-post("/users/:username")
+get("/users/:path_username",{controller: "users", action: "show"})
+get("/photos")
 end
